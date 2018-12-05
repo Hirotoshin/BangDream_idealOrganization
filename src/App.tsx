f1fed1125cf22734d89b41c494cb7072daba44bf
Hirotoshin <hirotoshin0211@gmail.com>
2018-12-05 18:57:43 +0900

    タイトルの変更

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class App extends React.Component {
+  public render() {
+    return (
+      <React.Fragment>
+        <h1>BangDream! 理想編成計算</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
