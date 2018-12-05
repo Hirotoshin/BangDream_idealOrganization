13174a09d59b9e22517bb8bbfb0a12a4040e5a68
Hirotoshin <hirotoshin0211@gmail.com>
2018-12-05 19:35:37 +0900

    storeの追加

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { logger } from "redux-logger";
+import App from "./App";
+import "./index.css";
+import MemberReducer from "./reducer/MemberReducer";
+import registerServiceWorker from "./registerServiceWorker";
+
+const store = createStore(MemberReducer, applyMiddleware(logger));
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
